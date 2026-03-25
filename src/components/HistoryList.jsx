85f016a056ec75dfe54d31ef700a80117b87d7eb
VinaPotenztech <vina@potenztech.com>
2026-03-25 19:07:09 +0530

    code format

--- src/components/HistoryList.jsx
+++ src/components/HistoryList.jsx
@@ -1,3 +1,4 @@
+
 import { api } from "../lib/api";
 import { useAuth } from "../context/AuthContext";
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; // 👈 import this
+import { BrowserRouter } from "react-router-dom"; 
 import { AuthProvider } from "./context/AuthContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
      <React.StrictMode>
-   <BrowserRouter>          {/* 👈 wrap the app */}
-      <AuthProvider>         {/* make sure AuthProvider wraps App */}
+   <BrowserRouter>         
+      <AuthProvider>         
         <App />
       </AuthProvider>
     </BrowserRouter>
